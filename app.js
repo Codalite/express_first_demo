bf29f6048c909a2a14b9e78801dd33fcd3f2ee10
codalite miki <alainmiki@hotmail.com>
2025-08-13 11:05:26 +0100

    cleaned up the structure for better codebase and deleted the user.js file

--- app.js
+++ app.js
@@ -1,34 +1,9 @@
 import express,{Router} from "express"
-import route from "./users.js"
+
 const app= express()
 app.use(express.json())
 app.use(express.urlencoded())
 app.use(express.text())
-
-
-
-app.use("/api",route)
-
-app.get("/",(req,res)=>{
-    return res.status(200).send({"data":"hello there coder"})
-})
-
-app.post("/",(req,res)=>{
-    console.log(req.body);
-    return res.status(201).send("created")
-    
-})
-app.put("/:id",(req,res)=>{
-    console.log(req.params.id);
-    console.log(req.query.user);
-    return res.status(201).send("created")
-    
-})
-app.delete("/:id",(req,res)=>{
-    console.log(req.body);
-    return res.status(201).send("created")
-    
-})
 
 
 app.listen(8080,(err)=>{
